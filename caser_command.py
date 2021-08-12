50160818ea3b4f00d68564591c1445a435e1be7f
Zach Lome <zach.lome@amount.com>
2021-08-12 09:48:42 -0500

    feat/refactor(commands): add word command and move things to settings.

--- caser_command.py
+++ caser_command.py
@@ -2,44 +2,27 @@ import re
 import sublime
 import sublime_plugin
 import types
-
-s = sublime.load_settings("caser.sublime-settings")
-
-snake_case_pattern = re.compile(r'(?<!^)(?=[A-Z])')
-
-def camel_case(string): # this will be rebound to the base string
-    return ''.join(word.title() for word in string.split('_'))
-
-def snake_case(string): # this will be rebound to the base string
-    return snake_case_pattern.sub('_', string).lower()
+from . import modifiers
 
 class CaserCommand(sublime_plugin.TextCommand):
-    def modify_regions(self, edit, modifier, alt_method = None):
-        sublime.status_message("running")
+    def modify_regions(self, edit, modifier):
         regions = self.view.sel()
+        use_whole_file =  modifiers.SETTINGS.get("use_entire_file_if_no_selection", True)
 
         selections = []
         for region in regions:
-            selected_entire_file = False
-            if region.empty():
-                if len(regions) > 1:
-                    continue
-                elif s.get("use_entire_file_if_no_selection", True):
-                    selection = sublime.Region(0, self.view.size())
-                    selected_entire_file = True
-                else:
-                    selection = region
+            if region.empty() and len(regions) == 1 and use_whole_file:
+                selection = sublime.Region(0, self.view.size())
             else:
                 selection = region
 
             base_str = self.view.substr(selection)
 
-            if alt_method is not None:
-                modified_str = alt_method(base_str)
-            else:
-                modified_str = getattr(base_str, modifier)()
+            modified_str = getattr(modifiers, modifier)(base_str)
 
             self.view.replace(edit, selection, modified_str)
+
+        sublime.status_message('Casing changed')
 
 class CaserDowncaseCommand(CaserCommand):
     def run(self, edit):
@@ -55,9 +38,12 @@ class CaserTitleizeCommand(CaserCommand):
 
 class CaserSnakeCaseCommand(CaserCommand):
     def run(self, edit):
-        self.modify_regions(edit, 'snake_case', snake_case)
+        self.modify_regions(edit, 'snake_case')
 
 class CaserCamelCaseCommand(CaserCommand):
     def run(self, edit):
-        self.modify_regions(edit, 'snake_case', camel_case)
+        self.modify_regions(edit, 'camel_case')
 
+class CaserWordCommand(CaserCommand):
+    def run(self, edit):
+        self.modify_regions(edit, 'snake_to_words')
